aef89c3e2667966293a64e4577676eef782328e8
Andy Yip <andy.yip@oocl.com>
2025-09-08 11:02:51 +0800

    feat: a person introduce himself

--- src/main/java/oo/Person.java
+++ src/main/java/oo/Person.java
@@ -1,4 +1,17 @@
 package oo;
 
 public class Person {
+    private final int id;
+    private final String name;
+    private final int age;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce() {
+        return "My name is %s. I am %d years old.".formatted(name, age);
+    }
 }
--- src/test/java/oo/step1/PersonTest.java
+++ src/test/java/oo/step1/PersonTest.java
@@ -1,9 +1,18 @@
 package oo.step1;
 
+import oo.Person;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PersonTest {
 
 
     // when introduce a person, then return a message with name and age
+    @Test
+    void should_introduce_self_when_introduce_given_a_person() {
+        Person person = new Person(1, "Tom", 18);
+        assertEquals("My name is Tom. I am 18 years old.", person.introduce());
+    }
 
     // when equals two same id person then return true
 
